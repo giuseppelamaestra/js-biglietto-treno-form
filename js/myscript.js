7b58868e68b558d15b18094e1372d72b0f4b3501
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-05-15 17:33:41 +0200

    added input 1 and input 2 and button

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+// Il numero di chilometri da percorrere
+//Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+//il prezzo del biglietto è definito in base ai km (0.233 € al km)
+//va applicato uno sconto del 19.4% per i minorenni//
+//va applicato uno sconto del 37.7% per gli over 65. //
+
+const input1 = document.getElementById("one");
+const input2 = document.getElementById("two");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+     const kilometers = input1.value
+     const passengerAge = input2.value
+     const KilomersPrice = kilometers * 0.233; 
+
+    if
+    
+ 
+})
